1d6fdc4c23f0967bb6588263f179e64fe5337482
paco <pakokrew@gmail.com>
2022-01-19 20:22:21 +0100

    chakra

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
@@ -8,11 +9,13 @@ import './index.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
-    </RedwoodProvider>
+    <ChakraProvider>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </RedwoodProvider>
+    </ChakraProvider>
   </FatalErrorBoundary>
 )
 
