a1defd3ccfb4522c30cb8d6133dabfc35f1aee33
paco <pakokrew@gmail.com>
2022-01-22 12:27:49 +0100

    remove scaffold

--- web/src/pages/Scaffold/Image/ImagesPage/ImagesPage.tsx
+++
@@ -1,7 +0,0 @@
-import ImagesCell from 'src/components/Scaffold/Image/ImagesCell'
-
-const ImagesPage = () => {
-  return <ImagesCell />
-}
-
-export default ImagesPage
--- web/src/pages/Scaffold/Image/NewImagePage/NewImagePage.tsx
+++
@@ -1,7 +0,0 @@
-import NewImage from 'src/components/Scaffold/Image/NewImage'
-
-const NewImagePage = () => {
-  return <NewImage />
-}
-
-export default NewImagePage
