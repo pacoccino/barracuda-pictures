a7d052f95a3891f1308509ca7626a18e070b7549
paco <pakokrew@gmail.com>
2022-02-02 14:01:31 +0100

    skeleton on images

--- web/src/components/Image/ImagesCell/ImagesCell.tsx
+++ web/src/components/Image/ImagesCell/ImagesCell.tsx
@@ -2,6 +2,7 @@ import type { FindImages } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import Images from 'src/components/Image/Images'
+import { Skeleton, Box, Wrap, WrapItem } from '@chakra-ui/react'
 
 export const QUERY = gql`
   query FindImages($filter: ImageFilters!) {
@@ -13,11 +14,19 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => (
+  <Wrap m={2} ml={0} spacing={0.5}>
+    {[0, 1, 2].map((i) => (
+      <WrapItem key={i}>
+        <Skeleton>
+          <Box h={250} w={250}></Box>
+        </Skeleton>
+      </WrapItem>
+    ))}
+  </Wrap>
+)
 
-export const Empty = () => {
-  return <div className="rw-text-center">{'No images yet. '}</div>
-}
+export const Empty = () => <Box>No images</Box>
 
 export const Failure = ({ error }: CellFailureProps) => (
   <div className="rw-cell-error">{error.message}</div>
