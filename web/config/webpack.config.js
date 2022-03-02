a45560344115a91d61f3410a1088259a8866626e
paco <pakokrew@gmail.com>
2022-03-02 17:36:33 +0100

    webpack dev 0.0.0.0

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -3,6 +3,7 @@ const escapeRegExp = require('lodash.escaperegexp')
 /** @returns {import('webpack').Configuration} Webpack Configuration */
 module.exports = (config, { mode }) => {
   if (mode === 'development') {
+    config.devServer.host = '0.0.0.0'
     config.devServer.proxy['/s3'] = {
       target: 'http://[::1]:9000',
       pathRewrite: {
